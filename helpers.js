cf7a35a21c666ade8250a3de349ddbc15ea9e7e5
Marcus Ortense <marcus.ortense@outlook.com>
2017-02-08 11:36:22 -0200

    remove unnecessary helper file

--- helpers.js
+++
@@ -1,9 +0,0 @@
-import supertest from 'supertest'
-import Joi       from 'joi'
-import joiAssert from 'joi-assert'
-import chai      from 'chai'
-
-global.expect    = chai.expect
-global.supertest = supertest
-global.Joi       = Joi
-global.joiAssert = joiAssert
